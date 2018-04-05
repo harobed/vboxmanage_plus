55ba7abf5aac7ac903009583da6513c677a052e5
Stéphane Klein <contact@stephane-klein.info>
2018-04-05 23:51:51 +0200

    Add list orphaned hdds command

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # Virtualbox VBoxManage helper
 
-Execute this command to clean all Virtualbox orphaned hdds
+Execute this command to list all Virtualbox orphaned hdds:
+
+```
+$ vboxmanage_plus list orphaned hdds
+```
+
+
+Execute this command to clean all Virtualbox orphaned hdds:
 
 ```
 $ vboxmanage_plus remove orphaned hdds
 ```
+
 
 ## Execute tests
 
--- vboxmanage_plus/__init__.py
+++ vboxmanage_plus/__init__.py
@@ -17,6 +17,11 @@ def parse_vbox_manage_list_hdd(data):
 
 
 def remove_orphan_hdds():
+    for hdd in list_orphan_hdds():
+        remove_hdd(hdd['uuid'])
+
+
+def list_orphan_hdds():
     p = subprocess.Popen(
         "VBoxManage list hdds",
         shell=True,
@@ -26,9 +31,11 @@ def remove_orphan_hdds():
     out, err = p.communicate()
     errcode = p.returncode
 
-    for hdd in parse_vbox_manage_list_hdd(out):
-        if hdd['state'] == 'inaccessible':
-            remove_hdd(hdd['uuid'])
+    return [
+        hdd for hdd in parse_vbox_manage_list_hdd(out)
+        if hdd['state'] == 'inaccessible'
+    ]
+
 
 def remove_rdd(uuid):
     print("Remove medium %s" % uuid)
@@ -46,19 +53,33 @@ def cli():
     remove_parser = root_subparsers.add_parser("remove")
     remove_subparsers = remove_parser.add_subparsers(dest="remove_subparsers")
 
-    orphaned_parser = remove_subparsers.add_parser("orphaned")
-    orphaned_subparsers = orphaned_parser.add_subparsers(dest="orphaned_subparsers")
+    orphaned_remove_parser = remove_subparsers.add_parser("orphaned")
+    orphaned_remove_subparsers = orphaned_remove_parser.add_subparsers(dest="orphaned_remove_subparsers")
 
-    hdds_parser = orphaned_subparsers.add_parser("hdds")
+    hdds_orphaned_remove_parser = orphaned_remove_subparsers.add_parser("hdds")
+
+    list_parser = root_subparsers.add_parser("list")
+    list_subparsers = list_parser.add_subparsers(dest="list_subparsers")
+
+    orphaned_list_parser = list_subparsers.add_parser("orphaned")
+    orphaned_list_subparsers = orphaned_list_parser.add_subparsers(dest="orphaned_list_subparsers")
+
+    hdds_orphaned_list_parser = orphaned_list_subparsers.add_parser("hdds")
 
     args = root_parser.parse_args()
+    print(args)
 
-    if (
-        (args.root_subparsers == 'remove') and
-        (args.remove_subparsers == 'orphaned') and
-        (args.orphaned_subparsers == 'hdds')
-    ):
-        remove_orphan_hdds()
+    if args.root_subparsers == 'list':
+        if args.list_subparsers == 'orphaned':
+            if args.orphaned_list_subparsers == 'hdds':
+                for hdd in list_orphan_hdds():
+                    print("%s - %s" % (hdd['uuid'], hdd['location']))
+
+    elif args.root_subparsers == 'remove':
+        if args.remove_subparsers == 'orphaned':
+            if args.orphaned_remove_subparsers == 'hdds':
+                remove_orphan_hdds()
+
 
 if __name__ == '__main__':
     cli()
